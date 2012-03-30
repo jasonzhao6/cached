a1fca8823b8675b4c146bb42da809b58610043f2
Jason Zhao <jasonzhao6@gmail.com>
2012-03-29 21:37:16 -0700

    made style stripping cleaning while preserving it

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -42,15 +42,7 @@ class ArticlesController < ApplicationController
   
   def show
     article = Article.find params[:id]
-    # these filters should eventually be applied on create.. once they are more finalized
-    article.body.gsub! /color:[a-z0-9, -.]+;/i, ''
-    article.body.gsub! /font-family:[a-z0-9, -.]+;/i, ''
-    article.body.gsub! /font-size:[a-z0-9, -.]+;/i, ''
-    article.body.gsub! /line-height:[a-z0-9, -.]+;/i, ''
-    article.body.gsub! /margin-top:[a-z0-9, -.]+;/i, ''
-    article.body.gsub! /margin-right:[a-z0-9, -.]+;/i, ''
-    article.body.gsub! /margin-bottom:[a-z0-9, -.]+;/i, ''
-    article.body.gsub! /margin-left:[a-z0-9, -.]+;/i, ''
+    article.body.gsub! /style="/, 'data-style="'
     article.body.gsub! /width="[0-9]{3,}"/, 'width="100%"'
     respond_with article
   end
