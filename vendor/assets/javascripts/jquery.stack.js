4c91581df6eff6d7b63995f33b2a913b921bd643
Jason Zhao <jasonzhao6@gmail.com>
2012-09-16 23:38:00 -0700

    revised stack to support resize

--- vendor/assets/javascripts/jquery.stack.js
+++ vendor/assets/javascripts/jquery.stack.js
@@ -3,28 +3,28 @@
   $.fn.extend({
     stack: function() {
       var $parent = $(this),
-          parentWidth = $parent.width() + $parent.width() % 2,
-          parentLeftPadding = parseInt($parent.css('padding-left'))
           $children = $parent.children(),
-          childrenWidth = $children.outerWidth(),
-          numColumns = Math.floor(parentWidth / childrenWidth),
-          columnHeights = new Array(numColumns),
-          initialize = function() {
-            resetcolumnHeights();
-            $children.each(function(i, child){ 
-              $child = $(child)
-              columnIndex = nextShortestColumn();
-              $child.css({
-                left: childrenWidth * columnIndex + parentLeftPadding,
-                position: 'absolute',
-                top: columnHeights[columnIndex],
-                width: childrenWidth
-              })
-              columnHeights[columnIndex] += $child.height();
+          parentWidth,
+          parentLeftPadding,
+          childrenWidth,
+          columnHeights,
+          reset = function() {
+            $parent.css({
+              height: '',
+              overflow: ''
             });
-            $parent.height(Math.max.apply(null, columnHeights));
-          },
-          resetcolumnHeights = function() {
+            $children.each(function(i, child){ 
+              $(child).css({
+                left: '',
+                position: '',
+                top: '',
+                width: ''
+              })
+            });
+            parentWidth = $parent.width();
+            parentLeftPadding = parseInt($parent.css('padding-left'));
+            childrenWidth = $children.outerWidth();
+            columnHeights = new Array(Math.round(parentWidth / childrenWidth));
             $.each(columnHeights, function(i){
               columnHeights[i] = 0;
             });
@@ -39,8 +39,27 @@
               }
             });
             return index;
+          },
+          resizeAction = function() {
+            reset();
+            $children.each(function(i, child){ 
+              $child = $(child)
+              columnIndex = nextShortestColumn();
+              columnHeight = columnHeights[columnIndex];
+              columnHeights[columnIndex] += $child.height();
+              $parent.css({
+                height: Math.max.apply(null, columnHeights),
+                overflow: 'hidden'
+              });
+              $child.css({
+                left: childrenWidth * columnIndex + parentLeftPadding,
+                position: 'absolute',
+                top: columnHeight,
+                width: childrenWidth
+              })
+            });
           };
-      initialize();
+      $(window).off('resize.stack').on('resize.stack', resizeAction).resize();
     }
   });
 })(jQuery);
